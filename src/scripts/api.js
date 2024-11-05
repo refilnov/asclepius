6848067ba39d59bf00df92b878ec0ab0fc0e24cc
Risqi <khoirulrizki029@gmail.com>
2024-11-05 17:41:52 +0700

    cloud build app engine add api

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,5 +1,5 @@
 // TODO: Silakan sesuaikan BASE URL dari endpoint Anda
-const BASE_URL = 'YOUR_BASE_URL';
+const BASE_URL = "https://cancer-583821042962.asia-southeast2.run.app";
 
 const ENDPOINT = {
   predict: `${BASE_URL}/predict`,
@@ -8,9 +8,9 @@ const ENDPOINT = {
 class PredictAPI {
   static async predict(data) {
     const response = await fetch(ENDPOINT.predict, {
-      method: 'POST',
+      method: "POST",
       body: data,
-      redirect: 'follow',
+      redirect: "follow",
     });
 
     const json = await response.json();
